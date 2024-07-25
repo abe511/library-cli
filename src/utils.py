bcbdb3d884a9c712a55e9ba7ee07e0d075263b2f
Art <absolutus11@gmail.com>
2024-07-25 17:20:03 +0400

    fix: rewrite generate_id()

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,6 @@
 import random
-from typing import List, Optional
+import string
+from typing import Optional
 
 def generate_id(length: Optional[int] = 8) -> str:
-  chars: str = "0123456789abcdefghijklmnopqrstuvwxyz"
-  end: int = len(chars) - 1;
-  id_string: List[str] = []
-  for _ in range(length):
-    random_char: str = chars[random.randint(0, end)]
-    id_string.append(random_char)
-  return "".join(id_string)
+  return "".join(random.choices(string.digits + string.ascii_lowercase, k=length))
